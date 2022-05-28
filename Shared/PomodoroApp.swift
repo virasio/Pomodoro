30aad11dcb93812784e92cf17b7da9a68ba1a779
Victor Surikov <me@virasio.ru>
2022-05-28 09:07:12 +0300

    Initial Commit

---
+++ Shared/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Shared
+//
+//  Created by Victor Surikov on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
